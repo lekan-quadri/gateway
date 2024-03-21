bd7c56865615a33e649f27313fe8aa9bbff2992e
lekan-quadri <olalekanquadri@hotmail.com>
2024-03-21 13:10:44 +0100

    Update TransactionController.java

--- src/main/java/com/zenithbank/gateway/controller/TransactionController.java
+++ src/main/java/com/zenithbank/gateway/controller/TransactionController.java
@@ -3,22 +3,18 @@ package com.zenithbank.gateway.controller;
 import com.zenithbank.gateway.model.Transaction;
 import com.zenithbank.gateway.model.TransactionRequest;
 import com.zenithbank.gateway.service.TransactionService;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/transactions")
-//@Api(tags = "Projects")
 public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
 
     @PostMapping
-//    @ApiOperation(value = "Get All Projects")
     public ResponseEntity<Transaction> initiateTransaction(@RequestBody TransactionRequest request) {
         Transaction transaction = transactionService.initiateTransaction(request);
         return ResponseEntity.ok(transaction);
